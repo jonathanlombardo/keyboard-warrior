93499dacbd5a8650db307adff4344fa7c2096147
jonathanlombardo <jonathanlombardo90@gmail.com>
2024-05-01 00:34:03 +0200

    create resource, migration and relation Lion, Plot, User

---
+++ app/Http/Controllers/LionController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lion;
+use Illuminate\Http\Request;
+
+class LionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lion  $lion
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lion $lion)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Lion  $lion
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lion $lion)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Lion  $lion
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lion $lion)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lion  $lion
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lion $lion)
+    {
+        //
+    }
+}
